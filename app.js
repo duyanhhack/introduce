578c8b3e289e33cd34c040a2758e5be8ab9ac5a1
= <=>
2021-08-21 15:53:12 +0700

    test github

--- app.js
+++ app.js
@@ -16,18 +16,3 @@ var marker = new google.maps.Marker({
 marker.setMap(map);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
-
-// Animation
-$(document).ready(function(){
-
-     $(window).scroll(function(){
-          $('.slideanim').each(function(){
-               var pos = $(this).offset().top
-
-               var winTop = $(window).scrollTop()
-               if(pos < winTop + 600){
-                    $(this).addClass("slide")
-               }
-          })
-     })
-})
